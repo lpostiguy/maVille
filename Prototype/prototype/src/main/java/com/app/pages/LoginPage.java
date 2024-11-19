c094d20cc0424642a81d776626ab743ce8882c76
Louis-Philippe Ostiguy <louis-philippe@liifer.com>
2024-11-18 20:02:51 -0500

    [PROTOTYPE] Ajout de 3 comptes résidents et 3 comptes intervenants hardcoded.

--- Prototype/prototype/src/main/java/com/app/pages/LoginPage.java
+++ Prototype/prototype/src/main/java/com/app/pages/LoginPage.java
@@ -29,24 +29,64 @@ public class LoginPage {
                 // TODO: Ajouter la logique qui compare les données rentrées par
                 //  le user et les données présentes dans une base de donnée
                 //  Mongo DB.
-                if (email.equals(GlobalUserInfo.emailRes) && Objects.equals(password, GlobalUserInfo.passwordRes)) {
+                if (email.equals(GlobalUserInfo.emailRes1) && Objects.equals(password, GlobalUserInfo.passwordRes1)) {
                     GlobalUserInfo.setCurrentRole("RESIDENT");
                     System.out.println("Connection réussie");
-                    return new Resident(GlobalUserInfo.firstNameRes,
-                        GlobalUserInfo.lastNameRes, GlobalUserInfo.emailRes,
-                        GlobalUserInfo.phoneNumberRes,
-                        GlobalUserInfo.dateOfBirthRes,
-                        GlobalUserInfo.homeAddressRes,
-                        passwordEncryption.encrypt(GlobalUserInfo.passwordRes));
-                } else if (email.equals(GlobalUserInfo.emailInt) && Objects.equals(password, GlobalUserInfo.passwordInt)) {
+                    return new Resident(GlobalUserInfo.firstNameRes1,
+                        GlobalUserInfo.lastNameRes1, GlobalUserInfo.emailRes1,
+                        GlobalUserInfo.phoneNumberRes1,
+                        GlobalUserInfo.dateOfBirthRes1,
+                        GlobalUserInfo.homeAddressRes1,
+                        passwordEncryption.encrypt(GlobalUserInfo.passwordRes1));
+                }
+                else if (email.equals(GlobalUserInfo.emailRes2) && Objects.equals(password, GlobalUserInfo.passwordRes2)) {
+                    GlobalUserInfo.setCurrentRole("RESIDENT");
+                    System.out.println("Connection réussie");
+                    return new Resident(GlobalUserInfo.firstNameRes2,
+                        GlobalUserInfo.lastNameRes2, GlobalUserInfo.emailRes2,
+                        GlobalUserInfo.phoneNumberRes2,
+                        GlobalUserInfo.dateOfBirthRes2,
+                        GlobalUserInfo.homeAddressRes2,
+                        passwordEncryption.encrypt(GlobalUserInfo.passwordRes2));
+                }
+                else if (email.equals(GlobalUserInfo.emailRes3) && Objects.equals(password, GlobalUserInfo.passwordRes3)) {
+                    GlobalUserInfo.setCurrentRole("RESIDENT");
+                    System.out.println("Connection réussie");
+                    return new Resident(GlobalUserInfo.firstNameRes3,
+                        GlobalUserInfo.lastNameRes3, GlobalUserInfo.emailRes3,
+                        GlobalUserInfo.phoneNumberRes3,
+                        GlobalUserInfo.dateOfBirthRes3,
+                        GlobalUserInfo.homeAddressRes3,
+                        passwordEncryption.encrypt(GlobalUserInfo.passwordRes3));
+                }
+                else if (email.equals(GlobalUserInfo.emailInt1) && Objects.equals(password, GlobalUserInfo.passwordInt1)) {
                     GlobalUserInfo.setCurrentRole("INTERVENANT");
                     System.out.println("Connection réussie");
-                    return new Intervenant(GlobalUserInfo.firstNameInt,
-                        GlobalUserInfo.lastNameInt, GlobalUserInfo.emailInt,
-                        GlobalUserInfo.entityTypeInt,
-                        GlobalUserInfo.cityIdInt,
-                        passwordEncryption.encrypt(GlobalUserInfo.passwordInt));
-                } else {
+                    return new Intervenant(GlobalUserInfo.firstNameInt1,
+                        GlobalUserInfo.lastNameInt1, GlobalUserInfo.emailInt1,
+                        GlobalUserInfo.entityTypeInt1,
+                        GlobalUserInfo.cityIdInt1,
+                        passwordEncryption.encrypt(GlobalUserInfo.passwordInt1));
+                }
+                else if (email.equals(GlobalUserInfo.emailInt2) && Objects.equals(password, GlobalUserInfo.passwordInt2)) {
+                    GlobalUserInfo.setCurrentRole("INTERVENANT");
+                    System.out.println("Connection réussie");
+                    return new Intervenant(GlobalUserInfo.firstNameInt2,
+                        GlobalUserInfo.lastNameInt2, GlobalUserInfo.emailInt2,
+                        GlobalUserInfo.entityTypeInt2,
+                        GlobalUserInfo.cityIdInt2,
+                        passwordEncryption.encrypt(GlobalUserInfo.passwordInt2));
+                }
+                else if (email.equals(GlobalUserInfo.emailInt3) && Objects.equals(password, GlobalUserInfo.passwordInt3)) {
+                    GlobalUserInfo.setCurrentRole("INTERVENANT");
+                    System.out.println("Connection réussie");
+                    return new Intervenant(GlobalUserInfo.firstNameInt3,
+                        GlobalUserInfo.lastNameInt3, GlobalUserInfo.emailInt3,
+                        GlobalUserInfo.entityTypeInt3,
+                        GlobalUserInfo.cityIdInt3,
+                        passwordEncryption.encrypt(GlobalUserInfo.passwordInt3));
+                }
+                else {
                     System.out.println("[1] Retour");
                     System.out.println("[2] Réessayer");
                     System.out.println("Mauvais mot de passe ou mauvaise " +
--- Prototype/prototype/src/main/java/com/app/utils/GlobalUserInfo.java
+++ Prototype/prototype/src/main/java/com/app/utils/GlobalUserInfo.java
@@ -4,25 +4,64 @@ public class GlobalUserInfo {
 
     public static String currentRole = "";
 
-    // Hard Coded Resident
-    public static String firstNameRes = "Test First Name";
-    public static String lastNameRes = "Test Last Name";
-    public static String emailRes = "test@resident.com";
-    public static String phoneNumberRes = "514-888-9090";
-    public static String dateOfBirthRes = "02-07-2001";
-    public static String homeAddressRes = "564, test Avenue, Montréal";
-    public static String passwordRes = "Test12345";
-    public static String idRes = "34ed6d7wdw9ee3451";
+    // Hard Coded Resident 1
+    public static String firstNameRes1 = "Jacob";
+    public static String lastNameRes1 = "Wayne";
+    public static String emailRes1 = "jacob.wayne@resident.com";
+    public static String phoneNumberRes1 = "514-888-9090";
+    public static String dateOfBirthRes1 = "02-07-2001";
+    public static String homeAddressRes1 = "564, wayne Avenue, Montréal";
+    public static String passwordRes1 = "Wayne12345";
+    public static String idRes1 = "34ed6d7wdw9ee3451";
+
+    // Hard Coded Resident 2
+    public static String firstNameRes2 = "Marc";
+    public static String lastNameRes2 = "Jacobs";
+    public static String emailRes2 = "marc.jacobs@resident.com";
+    public static String phoneNumberRes2 = "516-886-9312";
+    public static String dateOfBirthRes2 = "12-07-2006";
+    public static String homeAddressRes2 = "464, Jacobs Avenue, Montréal";
+    public static String passwordRes2 = "Marc12345";
+    public static String idRes2 = "5463dd4r6g4jcxh34nk34";
+
+    // Hard Coded Resident 3
+    public static String firstNameRes3 = "Max";
+    public static String lastNameRes3 = "Ronald";
+    public static String emailRes3 = "max.ronald@resident.com";
+    public static String phoneNumberRes3 = "214-348-1390";
+    public static String dateOfBirthRes3 = "01-12-2011";
+    public static String homeAddressRes3 = "894, Ronald Avenue, Montréal";
+    public static String passwordRes3 = "Max12345";
+    public static String idRes3 = "uhfeo782xneoz8m22";
 
 
-    // Hard Code Intervenant
-    public static String firstNameInt = "Test First Name";
-    public static String lastNameInt = "Test Last Name";
-    public static String emailInt = "test@intervenant.com";
-    public static String entityTypeInt = "Entreprise";
-    public static String cityIdInt = "273283";
-    public static String passwordInt = "Test12345";
-    public static String idInt = "de56723vd82h3ve3";
+
+    // Hard Code Intervenant 1
+    public static String firstNameInt1 = "James";
+    public static String lastNameInt1 = "Williams";
+    public static String emailInt1 = "james.williams@intervenant.com";
+    public static String entityTypeInt1 = "Entreprise";
+    public static String cityIdInt1 = "273283";
+    public static String passwordInt1 = "James12345";
+    public static String idInt1 = "de56723vd82h3ve3";
+
+    // Hard Code Intervenant 2
+    public static String firstNameInt2 = "George";
+    public static String lastNameInt2 = "Mackenzie";
+    public static String emailInt2 = "george.mackenzie@intervenant.com";
+    public static String entityTypeInt2 = "Entrepreneur";
+    public static String cityIdInt2 = "742392";
+    public static String passwordInt2 = "George12345";
+    public static String idInt2 = "348782ehdjscn2302";
+
+    // Hard Code Intervenant 3
+    public static String firstNameInt3 = "Bruce";
+    public static String lastNameInt3 = "Cuban";
+    public static String emailInt3 = "bruce.cuban@intervenant.com";
+    public static String entityTypeInt3 = "Entreprise";
+    public static String cityIdInt3 = "99384652";
+    public static String passwordInt3 = "Bruce12345";
+    public static String idInt3 = "3728e2ynxdukjs928dsiwjl";
 
     public static void setCurrentRole(String currentRole) {
         GlobalUserInfo.currentRole = currentRole;
